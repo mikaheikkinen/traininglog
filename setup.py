fa8c12371e895b93e4cf684e69ab55995e977453
Mika Heikkinen <mika.heikkinen1@gmail.com>
2015-09-19 14:10:56 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='traininglog',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='mika',
+    author_email='mika.heikkinen1@gmail.com',
+    description='Traininglog app'
+)
