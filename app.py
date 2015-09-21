2553c30281c4dff264450e58db44a45f5e33c43f
Mika Heikkinen <mika.heikkinen1@gmail.com>
2015-09-21 14:57:46 +0300

    testin github polling jenkins

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World testing jenkins!"
+    return "Hello World polling my ass of!"
 
 if __name__ == '__main__':
     app.run()
